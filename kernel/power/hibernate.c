51815536dc139c60f97b8fba76077969274c1718
Atul Raut <araut@codeaurora.org>
2018-05-23 11:36:53 -0700

    power:hibernate: KPI marker for Hibernation Success
    Add KPI Marker for hibernation failure and success
    needed for perf image.
    
    Change-Id: I90a4e503c75ccb1d0edb3d26f7f8d5f08dafe95e
    Signed-off-by: Atul Raut <araut@codeaurora.org>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -29,6 +29,7 @@
 #include <linux/ctype.h>
 #include <linux/genhd.h>
 #include <trace/events/power.h>
+#include <soc/qcom/boot_stats.h>
 
 #include "power.h"
 
@@ -470,6 +471,7 @@ static int resume_target_kernel(bool platform_mode)
 	touch_softlockup_watchdog();
 
 	syscore_resume();
+	place_marker("PM: Image Restoration failed!");
 
  Enable_irqs:
 	local_irq_enable();
@@ -704,6 +706,7 @@ int hibernate(void)
 		pm_restore_gfp_mask();
 	} else {
 		pr_debug("PM: Image restored successfully.\n");
+		place_marker("PM: Image restored!");
 	}
 
  Free_bitmaps:
